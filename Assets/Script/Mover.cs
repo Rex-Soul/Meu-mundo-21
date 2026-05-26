f2c192515e7f55173969e8d03843b75822f52821
Rex-Soul <yuriyam407@gmail.com>
2026-05-26 20:39:47 -0300

    oliii

--- Assets/Script/Mover.cs
+++ Assets/Script/Mover.cs
@@ -2,20 +2,49 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] Vector3 _moverPlayer;
+    [SerializeField] Vector3 _movePlayer;
+    [SerializeField] CharacterController _controller;
+
+    [SerializeField] Vector3 playerVelocity;
+    [SerializeField] bool groundedPlayer;
+    [SerializeField] float playerSpeed = 2.0f;
+    [SerializeField] float jumpHeight = 1.0f;
+    [SerializeField] float gravityValue = -9.81f;
     void Start()
     {
-        InputMoverPlayer();
+        
+        _controller=GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InputMovePlayer();
+        MovendoPlayer();
     }
-    void InputMoverPlayer()
+    void InputMovePlayer()
     {
-        _moverPlayer.x = Input.GetAxis("Horizontal");
-        _moverPlayer.z = Input.GetAxis("Vertical");
+        _movePlayer.x = Input.GetAxis("Horizontal");
+        _movePlayer.z = Input.GetAxis("Vertical");
+    }
+    void MovendoPlayer()
+    {
+        groundedPlayer = _controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _controller.Move(_movePlayer * Time.deltaTime * playerSpeed);
+        if (_movePlayer != Vector3.zero)
+        {
+            gameObject.transform.forward = _movePlayer;
+        }
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        _controller.Move(playerVelocity * Time.deltaTime);
     }
 }
